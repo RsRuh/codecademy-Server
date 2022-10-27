0f053e9ca23cd30a316a67faa4d484eba65fac2d
RsRuh <rshridoysr@gmail.com>
2022-10-27 10:24:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const coursesData = require('./data/courses.json') 
+
+app.get('/', (req, res)=>{
+    res.send('News API Running');
+})
+
+app.get('/courses', (req, res)=>{
+    res.send(coursesData);
+})
+
+app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id;
+    const perCourse = coursesData.find((courses)=> courses._id == id);
+
+    res.send(perCourse);
+})
+
+app.listen(port, ()=>{
+    console.log('Codecademy is running');
+})
